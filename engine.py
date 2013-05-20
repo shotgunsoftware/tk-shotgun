02ce8f0bfde99308fb73506d93f7b0476001bea1
manne ohrstrom <manne@shotgunsoftware.com>
2013-05-20 20:31:18 +0100

    #21396 re-enabled pyqt

--- engine.py
+++ engine.py
@@ -86,7 +86,7 @@ class ShotgunEngine(Engine):
             qt_application.setStyleSheet(css) 
             
             # when the QApp starts, initialize our task code 
-            QtCore.QTimer.singleShot(0, t, QtCore.SLOT('run_command()'))
+            QtCore.QTimer.singleShot(0, t.run_command )
                
             # and ask the main app to exit when the task emits its finished signal
             t.finished.connect(qt_application.quit )
@@ -167,40 +167,40 @@ class ShotgunEngine(Engine):
                 self.log_warning("Error setting up pyside. Pyside based UI support will not "
                                  "be available: %s" % e)
         
-#        if not self._has_ui:
-#            try:
-#                from PyQt4 import QtCore, QtGui
-#                import PyQt4
-#                
-#                # a simple dialog proxy that pushes the window forward
-#                class ProxyDialogPyQt(QtGui.QDialog):
-#                    def show(self):
-#                        QtGui.QDialog.show(self)
-#                        self.activateWindow()
-#                        self.raise_()
-#                
-#                    def exec_(self):
-#                        self.activateWindow()
-#                        self.raise_()
-#                        # the trick of activating + raising does not seem to be enough for
-#                        # modal dialogs. So force put them on top as well.                        
-#                        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | self.windowFlags())
-#                        QtGui.QDialog.exec_(self)
-#                
-#                
-#                # hot patch the library to make it work with pyside code
-#                QtCore.Signal = QtCore.pyqtSignal     
-#                QtCore.Property = QtCore.pyqtProperty           
-#                base["qt_core"] = QtCore
-#                base["qt_gui"] = QtGui
-#                base["dialog_base"] = ProxyDialogPyQt
-#                self.log_debug("Successfully initialized PyQt located in %s." % PyQt4.__file__)
-#                self._has_ui = True
-#            except ImportError:
-#                pass
-#            except Exception, e:
-#                self.log_warning("Error setting up PyQt. PyQt based UI support will not "
-#                                 "be available: %s" % e)
+        if not self._has_ui:
+            try:
+                from PyQt4 import QtCore, QtGui
+                import PyQt4
+                
+                # a simple dialog proxy that pushes the window forward
+                class ProxyDialogPyQt(QtGui.QDialog):
+                    def show(self):
+                        QtGui.QDialog.show(self)
+                        self.activateWindow()
+                        self.raise_()
+                
+                    def exec_(self):
+                        self.activateWindow()
+                        self.raise_()
+                        # the trick of activating + raising does not seem to be enough for
+                        # modal dialogs. So force put them on top as well.                        
+                        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | self.windowFlags())
+                        QtGui.QDialog.exec_(self)
+                
+                
+                # hot patch the library to make it work with pyside code
+                QtCore.Signal = QtCore.pyqtSignal     
+                QtCore.Property = QtCore.pyqtProperty           
+                base["qt_core"] = QtCore
+                base["qt_gui"] = QtGui
+                base["dialog_base"] = ProxyDialogPyQt
+                self.log_debug("Successfully initialized PyQt %s located in %s." % (QtCore.PYQT_VERSION_STR, PyQt4.__file__))
+                self._has_ui = True
+            except ImportError:
+                pass
+            except Exception, e:
+                self.log_warning("Error setting up PyQt. PyQt based UI support will not "
+                                 "be available: %s" % e)
         
         return base
         
