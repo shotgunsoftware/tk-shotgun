ebdf357e048d2f961d00249b3ca5e6bf8dfff2ca
Eduardo Chauca <eduardo.chauca@autodesk.com>
2025-12-18 11:48:07 -0500

    comment update for outdate reference

--- engine.py
+++ engine.py
@@ -244,7 +244,8 @@ class ShotgunEngine(Engine):
             QtCore = base["qt_core"]
             QtGui = base["qt_gui"]
 
-            # tell QT4 to interpret C strings as utf-8
+            # On PySide2/PySide6 we patch QTextCodec with a do-nothing stub
+            # for setCodecForCStrings(), so this will have no effect.
             utf8 = QtCore.QTextCodec.codecForName("utf-8")
             QtCore.QTextCodec.setCodecForCStrings(utf8)
 
